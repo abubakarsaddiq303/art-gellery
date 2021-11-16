c864c69ab7ec251d7de29b9a0b22de659eef63bf
abubakarSaddiquee <abubakar.saddiq303@gmail.com>
2021-11-16 17:34:10 +0500

    16-11-2021

--- src/Fetchapi.js
+++ src/Fetchapi.js
@@ -1,38 +1,67 @@
 import react, { useState,useEffect } from "react";
+import { createClient } from 'pexels';
+import search1 from './magnifying-glass.png'
+import './App.css';
+
+
 
 
 function Fetchapi(){
 
-    // useEffect(() => {
-    //     fetchData();
+    useEffect(() => {
+        fetchData();
         
-    // }, []);
+    }, []);
     const [image1, setImage] = useState([]);
+    const [search, setSearch] = useState("");
 
     const fetchData = async () => {
-        const image = await fetch('https://api.thecatapi.com/v1/breeds/?api_key=9970f68a-4f40-4895-bb44-078c98f2974a')
+        // const image = await fetch('https://api.pexels.com/v1/curated?per_page=1"563492ad6f91700001000001929f59b90b0943c6bea996c79659a42a')
         
-        const Image = await image.json();
-        console.log(Image)
-        setImage(Image)
+        // const Image = await image.json();
+        // console.log(Image)
+        // setImage(Image)
+
+
+        const client = createClient('563492ad6f91700001000001929f59b90b0943c6bea996c79659a42a');
+        const query = 'bikes';
+
+        client.photos.search({ query, per_page: 80 }).then(photos => {setImage(photos);
+        console.log(photos)});
+
     }
+console.log(image1)
+console.log(search)
+
     return(
 
 <div>
-         my api<br/>
-     <button onClick={fetchData}>Fetch Api  </button>
-      <br/>
+        <h2 style={{textAlign:'center',textSize:'bold'}}> Art Gallery</h2><br/>
 
-
-       {image1.length!==0?image1.map(item=> {
+        <br/>
+         
+         <div className='SearchIcon'>
+         <img  src={search1} alt="search" class="icon" />
+     
+         <input
+  value={search}
+  onChange={e=>setSearch(e.target.value)}
+  placeholder={"Search Images"}
+/>
+</div>
+     {/* <button style={{marginLeft:'38rem'}} onClick={fetchData}><h5 >Fetch Api </h5> </button>
+      <br/> */}
+     
+       <div class="row">
+       {image1.length!==0?image1.photos.map(item=> {
            return(<div className="category" key={item.id}>
-               <p>{item.name}</p>
-            {/* <img alt="image" src={item.name} /> */}
+               {/* <p>{item.name}</p> */}
+            <img alt="image" src={item.src.landscape} />
         </div>
            )
        }):null}
-
-</div>
+         </div>
+      </div>
 
 
     )
@@ -44,7 +73,7 @@ function Fetchapi(){
 export default Fetchapi;
 
 
-
+ {/* 563492ad6f91700001000001929f59b90b0943c6bea996c79659a42a */}
 
 
 
