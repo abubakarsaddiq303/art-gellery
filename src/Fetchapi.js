24ff8c12713491ddafdb805d8064a18c6395b2a2
abubakarSaddiquee <abubakar.saddiq303@gmail.com>
2021-11-17 15:33:46 +0500

    17-11-2021

--- src/Fetchapi.js
+++ src/Fetchapi.js
@@ -1,128 +1,142 @@
-import react, { useState,useEffect } from "react";
-import { createClient } from 'pexels';
-import search1 from './magnifying-glass.png'
-import './App.css';
+import react, { useState, useEffect } from "react";
+import { createClient } from "pexels";
+import search1 from "./magnifying-glass.png";
+import search2 from "./gallery.png";
+import "./App.css";
 
+function Fetchapi() {
+  const [image1, setImage] = useState([]);
+  const [search, setSearch] = useState("");
 
+  const fetchData = async () => {
+    // const image = await fetch('https://api.pexels.com/v1/curated?per_page=1"563492ad6f91700001000001929f59b90b0943c6bea996c79659a42a')
 
+    // const Image = await image.json();
+    // console.log(Image)
+    // setImage(Image)
+    console.log("123");
+    const client = createClient(
+      "563492ad6f91700001000001929f59b90b0943c6bea996c79659a42a"
+    );
+    const query = search;
 
-function Fetchapi(){
+    await client.photos.search({ query, per_page: 80 }).then((photos) => {
+      setImage(photos);
+      console.log(photos);
+    });
+  };
 
-    useEffect(() => {
-        fetchData();
-        
-    }, []);
-    const [image1, setImage] = useState([]);
-    const [search, setSearch] = useState("");
+  console.log(image1);
+  console.log(search);
 
-    const fetchData = async () => {
-        // const image = await fetch('https://api.pexels.com/v1/curated?per_page=1"563492ad6f91700001000001929f59b90b0943c6bea996c79659a42a')
-        
-        // const Image = await image.json();
-        // console.log(Image)
-        // setImage(Image)
+  return (
+    <div>
+      <div className="header">
+        <img src={search2} alt="search" class="ArtIcon" />
+        <h2> Art Gallery</h2>
 
-
-        const client = createClient('563492ad6f91700001000001929f59b90b0943c6bea996c79659a42a');
-        const query = 'bikes';
-
-        client.photos.search({ query, per_page: 80 }).then(photos => {setImage(photos);
-        console.log(photos)});
-
-    }
-console.log(image1)
-console.log(search)
-
-    return(
-
-<div>
-        <h2 style={{textAlign:'center',textSize:'bold'}}> Art Gallery</h2><br/>
-
-        <br/>
-         
-         <div className='SearchIcon'>
-         <img  src={search1} alt="search" class="icon" />
-     
-         <input
-  value={search}
-  onChange={e=>setSearch(e.target.value)}
-  placeholder={"Search Images"}
-/>
-</div>
-     {/* <button style={{marginLeft:'38rem'}} onClick={fetchData}><h5 >Fetch Api </h5> </button>
-      <br/> */}
-     
-       <div class="row">
-       {image1.length!==0?image1.photos.map(item=> {
-           return(<div className="category" key={item.id}>
-               {/* <p>{item.name}</p> */}
-            <img alt="image" src={item.src.landscape} />
+        <div class="navbar">
+          <a href="#">About</a>
+          <a href="#">Contact</a>
         </div>
-           )
-       }):null}
-         </div>
       </div>
 
+      <br />
 
-    )
+      <br />
 
+      <div className="Search">
+        <img
+          src={search1}
+          alt="search"
+          class="icon"
+          onClick={(e) => fetchData()}
+        />
 
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder={"Search Images"}
+        />
+      </div>
 
+      <div className="All">
+        <ul className="Items ">
+          <li className="item">
+            <a className=" active" aria-current="page" href="#">
+              All
+            </a>
+          </li>
+          <li className="item">
+            <a className="link" href="#">
+              Travel
+            </a>
+          </li>
+          <li className="item">
+            <a className="link" href="#">
+              Nature
+            </a>
+          </li>
+          <li className="item">
+            <a class="link" href="#">
+              Photogaraphy
+            </a>
+          </li>
+          <li className="item">
+            <a className="link" href="#">
+              Fashion
+            </a>
+          </li>
+        </ul>
+      </div>
 
+      {/* <button style={{marginLeft:'38rem'}} onClick={fetchData}><h5 >Fetch Api </h5> </button>
+      <br/> */}
+
+      <div class="row">
+        {image1.length !== 0
+          ? image1.photos.map((item) => {
+              return (
+                <div className="category" key={item.id}>
+                  {/* <p>{item.name}</p> */}
+                  <img
+                    alt="image"
+                    src={item.src.medium}
+                    className="imagegrid"
+                  />
+                </div>
+              );
+            })
+          : null}
+      </div>
+    </div>
+  );
 }
 export default Fetchapi;
 
-
- {/* 563492ad6f91700001000001929f59b90b0943c6bea996c79659a42a */}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+{
+  /* 563492ad6f91700001000001929f59b90b0943c6bea996c79659a42a */
+}
 
 // import react, { useState } from "react";
 
-
-
 // function Fetchapi(){
 
-   
 //     const [data , setData] = useState ([])
 
 // const apiGet = () =>{
-
-
-    
 
 //     fetch('https://swapi.dev/api/people')
 //     .then(response => response.json())
 //     .then(abc => {
 
-        
 //         setData(abc)
 //      } );
 
-    
 // }
 
 // console.log(data)
 // return(
-
 
 // <div>
 
@@ -138,8 +152,6 @@ export default Fetchapi;
 // }):null}
 
 // </div>
-       
-       
 
 // </div>
 
